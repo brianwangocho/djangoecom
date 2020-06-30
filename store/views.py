47a3dcfc3565bcf218cfb9a97811efd6b463ab3d
brianwangocho <bwangocho@gmail.com>
2020-06-30 16:11:14 +0300

    fixed try exception

--- store/views.py
+++ store/views.py
@@ -36,11 +36,11 @@ def  cart(request):
         items =[]
         # if the cookie doesnt exist try catch
         try:
-             cart = json.loads(request.COOKIES['cart']
+            cart = json.loads(request.COOKIES['cart'])
         except:
-            cart={}
-        order = {'get_cart_total':0,'get_cart_items':0}
-        cartItems = order['get_cart_items']
+              cart={}
+    order = {'get_cart_total':0,'get_cart_items':0}
+    cartItems = order['get_cart_items']
     context={'items':items,'order':order,'cartItems':cartItems}
     return render(request,'store/cart.html',context)
 
